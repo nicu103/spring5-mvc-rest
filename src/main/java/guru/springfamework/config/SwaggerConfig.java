84574293dfc1dbe7b7d43f06d96becf74d1bad3f
Nicolae Taban <nicolae.taban@gmail.com>
2022-02-06 16:39:54 +0200

    Configures swagger descriptions

---
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package guru.springfamework.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+
+        Contact contact = new Contact();
+        return new OpenAPI()
+                .info(new Info().title("Spring MVC REST API")
+                        .description("Demo of RESTful web services with Spring")
+                        .version("v0.0.1")
+                        .termsOfService("Terms of Service URL goes here")
+                        .contact(contact)
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("More about SpringDoc and OpenAPI 3")
+                        .url("https://springdoc.org/"));
+    }
+}
